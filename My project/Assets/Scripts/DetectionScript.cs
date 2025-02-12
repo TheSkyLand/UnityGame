06cfacc262495d271874e5c73249ecdbe4364eb2
theskyland <aradioactiveplay@gmail.com>
2025-02-12 11:54:21 +0300

    fixing

--- My project/Assets/Scripts/DetectionScript.cs
+++ My project/Assets/Scripts/DetectionScript.cs
@@ -1,22 +1,58 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class DetectionScript : MonoBehaviour
 {
-    public EnemyScript Enemy; 
-    public Transform target;
+    public GameObject enemy;
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GetComponent<EnemyScript>();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
     }
-    private void OnTriggerEnter(Collider collider)
+    private void OnTriggerEnter(Collider player)
     {
+        if (player.tag == "GameController")
+        {
+
+            StartCoroutine(FollowPlayer());
+            Debug.Log("collided");
+            if (player.IsDestroyed() == true)
+            {
+                StopAllCoroutines();
+
+            }
+            else
+            {
+                StartCoroutine(FollowPlayer());
+            }
+        }
+    }
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.tag == "GameController")
+        {
+            StopAllCoroutines();
+        }
+        else
+        {
+            StartCoroutine(FollowPlayer());
+        }
+    }
+
+    public IEnumerator FollowPlayer()
+    {
+        var playerPos = player.transform.position;
+        playerPos.y = transform.position.y;
+        enemy.transform.LookAt(playerPos);
+        Debug.Log("worked");
+        yield return new WaitForSeconds(1);
+        Debug.Log("worked1");
     }
 }
--- My project/Assets/Scripts/PlayerScript.cs
+++ My project/Assets/Scripts/PlayerScript.cs
@@ -30,58 +30,27 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(PlayerHealth);
-        // forward movement + pressing W
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.forward * PlayerSpeed * Time.deltaTime;
-            Debug.Log("the w is pressed");
         }
-        // release w
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            Debug.Log("the w key has been released");
-        }
-        // backward movement + pressing S
         if (Input.GetKey(KeyCode.S))
         {
             transform.position -= transform.forward * PlayerSpeed * Time.deltaTime;
-            Debug.Log("the S is pressed");
         }
-        // releasing S
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            Debug.Log("the S key has been released");
-        }
-        // right? movement + pressing D
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += transform.right * PlayerSpeed * Time.deltaTime;
-            Debug.Log("the D is pressed");
         }
-        // releasing D
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            Debug.Log("the D key has been released");
-        }
-        // left? movement + pressing A
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= transform.right * PlayerSpeed  * Time.deltaTime;
-            Debug.Log("the A is pressed");
         }
-        // releasing A
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            Debug.Log("the A key has been released");
-        }
-
         if (Input.GetMouseButtonDown(0))   
         {
             Cursor.lockState = CursorLockMode.Confined;
             Debug.Log("Left mouse button locked");
         } 
-
         if (Input.GetKey(KeyCode.LeftShift)) {
             // Set current speed to run if shift is down
             PlayerSpeed = MaxSpeed;
@@ -91,18 +60,11 @@ public class PlayerScript : MonoBehaviour
             // Otherwise set current speed to walking speed
             PlayerSpeed = 5f;
         }
-
         if (Input.GetKeyDown(KeyCode.Keypad7))
         {
             Cursor.lockState = CursorLockMode.None;
             Debug.Log("Left mouse button unlocked");
         }
-        /*
-        if (Cursor.lockState == CursorLockMode.Locked)
-        {
-            float h = horizontalSpeed * Input.GetAxis("Mouse X");
-            
-        } */
 
 
         var playerPos = cursor.transform.position;
