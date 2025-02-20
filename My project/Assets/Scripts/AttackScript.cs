e38e4a3a68aa411f23c7226e83e8d0e006687c11
TheSkyLand <Aradioactiveplay@gmail.com>
2025-02-20 08:39:56 +0300

    update 1.21

--- My project/Assets/Scripts/AttackScript.cs
+++ My project/Assets/Scripts/AttackScript.cs
@@ -1,30 +1,33 @@
-
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.TextCore.Text;
-using UnityEngine.UIElements;
-using static UnityEngine.Color;
-using static UnityEngine.GraphicsBuffer;
-using System.Collections;
-using System.Collections.Generic;
-
 public class AttackScript : MonoBehaviour
 {
-
-    
+    private Animator anim;
+    public GameObject enemy;
+    public GameObject Player;
+    public GameObject weapon;
+    public EnemyAttack Attack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        anim.Play("Idle");
 
-
-    }   
+    }
+    private void OnTriggerEnter(Collider Player)
+    {
+        if (Player.tag == "GameController")
+        {
+            // If the GameObject has the same tag as specified, output this message in the console  
+            Player.GetComponent<PlayerScript>().ChangeHpBux(+Attack.BaseDamage);
+            anim.Play("KatanaAttack");
+        }
+    }
 
 }
 
